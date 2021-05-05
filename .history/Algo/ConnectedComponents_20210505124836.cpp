ad383e192c1072f857c59677f20ad0f77a6b23ac
manikanta2901 <manikanta2901@gmail.com>
2021-05-05 13:00:05 +0530

    History added after making changes

---
+++ .history/Algo/ConnectedComponents_20210505124836.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//DFS recursive implementation
+void DFSRecursive(int curr,vector<bool>& visited,vector<int> adj[],vector<int>& answer){
+    answer.push_back(curr);
+    visited[curr] = true;
+    for(auto it : adj[curr]){
+        if(!visited[it]){
+            DFSRecursive(it,visited,adj,answer);
+        }
+    }
+}   
+
+int main(){
+    int vertices,edges;
+    cout << "Enter the vertices and edges of the graph respectively " << endl;
+    cin >> vertices >> edges;
+    vector<int> adj[vertices + 1],answer; // one based indexing
+    cout << "Enter the edges of the graph" << endl;
+    for(int i = 1; i <= edges; i++){
+        int u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    cout << "Adjacency list of given graph is " << endl;
+    for(int i = 0; i <= vertices; i++){
+        cout << i << " : ";
+        for(auto j : adj[i]){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    int count = 0;
+
+    vector<bool> visited(vertices + 1,false);
+    for(int i = 1; i <= vertices; i++){
+        if(!visited[i]){
+            DFSRecursive(i,visited,adj,answer);
+            count++;
+        }
+    }
+
+
+    return 0;
+}
+
+
+
